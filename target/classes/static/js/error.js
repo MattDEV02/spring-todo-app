1134ba82cd3509aefed5ad105872f775748cfa8e
MattDEV02 <matteolambertucci3@gmail.com>
2021-04-08 22:49:19 +0200

    form ok

---
+++ target/classes/static/js/error.js
@@ -0,0 +1,2 @@
+const h1 = document.querySelector('h1');
+console.error(h1.innerText);
---
+++ target/classes/static/js/index.js
@@ -0,0 +1,7 @@
+const random = (Math.floor(Math.random() * 15) + 1);
+const
+   a = document.querySelector('#random'),
+   h1 =  document.querySelector('h1');
+console.log(h1.innerText);
+a.innerText += random;
+a.href += random;
