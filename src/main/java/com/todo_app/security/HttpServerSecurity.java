dd4df54ff87cfffe745ee7a57244b46ca840bfcc
MattDEV02 <matteolambertucci3@gmail.com>
2021-04-08 19:23:07 +0200

    reactjs actions OK

--- src/main/java/com/todo_app/security/HttpServerSecurity.java
+++ src/main/java/com/todo_app/security/HttpServerSecurity.java
@@ -9,20 +9,25 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.List;
+import com.todo_app.utils.index;
 
 
 @EnableWebSecurity
 @Configuration
-class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+class HttpServerSecurity extends WebSecurityConfigurerAdapter {
 
    @Override
-   protected void configure(HttpSecurity http) throws Exception {
-      http
-              .authorizeRequests()
-              .anyRequest().authenticated().and()
-              .httpBasic().and()
-              .csrf().disable()
-              .cors();
+   protected void configure(final HttpSecurity http) {
+      try {
+         http
+                 .authorizeRequests()
+                 .anyRequest().permitAll().and()
+                 .csrf().disable()
+                 .cors();
+      }
+      catch(final Exception e) {
+         index.handleException(e);
+      }
    }
 
    @Bean
