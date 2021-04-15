886135c1e6052368ad7ffa63941c85dfc78d538f
MattDEV02 <matteolambertucci3@gmail.com>
2021-04-15 14:02:30 +0200

    fix simple bug

--- src/main/java/com/todo_app/security/HttpServerSecurity.java
+++ src/main/java/com/todo_app/security/HttpServerSecurity.java
@@ -34,6 +34,9 @@ class HttpServerSecurity extends WebSecurityConfigurerAdapter {
    CorsConfigurationSource corsConfigurationSource() {
       CorsConfiguration configuration = new CorsConfiguration();
       configuration.setAllowedOrigins(List.of(
+              "http://127.0.0.1:3000",
+              "http://127.0.0.1:5000",
+              "http://127.0.0.1:8000",
               "http://localhost:3000",
               "http://localhost:5000",
               "http://localhost:8000"
