22081eaf6cb382640c51f8bffebb1e9b1f639ce9
MattDEV02 <matteolambertucci3@gmail.com>
2021-04-21 15:11:59 +0200

    styling calndar

--- src/main/reactjs/todo_app/backend/build/utils/web.js
+++ src/main/reactjs/todo_app/backend/build/utils/web.js
@@ -3,13 +3,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const config_1 = require("./config");
 const controller = express_1.Router();
+const defaultResponse = (res) => res.sendFile(config_1.filesData.file, config_1.filesData.options);
 controller
-    .all('/', (req, res) => res
-    .sendFile(config_1.filesData.file, config_1.filesData.options))
-    .all('/form', (req, res) => res
-    .sendFile(config_1.filesData.file, config_1.filesData.options))
-    .all('/calendar', (req, res) => res.
-    sendFile(config_1.filesData.file, config_1.filesData.options))
+    .all('/', (req, res) => defaultResponse(res))
+    .all('/form', (req, res) => defaultResponse(res))
+    .all('/calendar', (req, res) => defaultResponse(res))
+    .all('/chart', (req, res) => defaultResponse(res))
     .all('*', (req, res) => res
     .status(404)
     .redirect('/'));
--- src/main/reactjs/todo_app/backend/src/utils/web.ts
+++ src/main/reactjs/todo_app/backend/src/utils/web.ts
@@ -4,21 +4,25 @@ import { filesData } from './config';
 
 const controller: Router = Router();
 
+const defaultResponse = (res: Response): void => res.sendFile(filesData.file, filesData.options);
+
+
 controller
 	.all('/',
 		(req: Request, res: Response): void =>
-			res
-				.sendFile(filesData.file, filesData.options)
+			defaultResponse(res)
 	)
 	.all('/form',
 		(req: Request, res: Response): void =>
-			res
-				.sendFile(filesData.file, filesData.options)
+			defaultResponse(res)
 	)
 	.all('/calendar',
 		(req: Request, res: Response): void =>
-			res.
-				sendFile(filesData.file, filesData.options)
+			defaultResponse(res)
+	)
+	.all('/chart',
+		(req: Request, res: Response): void =>
+			defaultResponse(res)
 	)
 	.all('*', (req: Request, res: Response) =>
 		res
